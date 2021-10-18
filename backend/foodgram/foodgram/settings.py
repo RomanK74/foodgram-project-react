5371f95cb8492a329a193a8d7b59dc2d61bda715
RomanK74 <ganry12@gmail.com>
2021-10-18 19:01:21 +0300

    diploy

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('ALLOWED_HOSTS', '*').split(', ')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend', '0.0.0.0']
 
 os.getenv('ALLOWED_HOSTS', '*').split(', ')
 
