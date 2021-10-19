b5bd70c2623dc3b6673477f97451d11b70577ece
RomanK74 <ganry12@gmail.com>
2021-10-19 19:23:27 +0300

    try

--- backend/recipes/paginator.py
+++ backend/recipes/paginator.py
@@ -2,7 +2,4 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class RecipesPagination(PageNumberPagination):
-
     page_size_query_param = "limit"
-#     page_size = 6
-#     page_query_param = "page"
