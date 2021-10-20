582d2b9a3433697c1e61def9ca5a3112f2c70985
RomanK74 <ganry12@gmail.com>
2021-10-20 16:13:43 +0300

    update settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['178.154.254.67', 'food_and_hedonism.co.vu']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
